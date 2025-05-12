b5c505f7d2a2fdca06c18a25022f1fb10e4ba4fc
ikreishi <vadas25@yandex.ru>
2025-05-12 10:17:16 +0500

    added first page

---
+++ src/main/java/com/zkspringfromscratch/ZkSpringFromScratch.java
@@ -0,0 +1,11 @@
+package com.zkspringfromscratch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZkSpringFromScratch {
+    public static void main(String[] args) {
+        SpringApplication.run(ZkSpringFromScratch.class, args);
+    }
+}
